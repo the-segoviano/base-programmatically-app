776e43865bbec7d5cc84087b617c543449591d8a
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-13 20:18:41 -0600

    Initial Commit

---
+++ Base Programmatically App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Base Programmatically App
+//
+//  Created by Luis Segoviano on 13/01/20.
+//  Copyright © 2020 The Segoviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
