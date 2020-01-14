c0e4c24707914c77df41064a0c3e0be584ce5db2
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-14 11:18:02 -0600

    Agregando vistas de referencia.

--- Base Programmatically App/ViewController.swift
+++ Base Programmatically App/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Here is where everything start!"
+        label.textAlignment = .center
+        self.view.addSubview(label)
+        label.widthAnchor.constraint(equalToConstant: self.view.bounds.width).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
 
 }
